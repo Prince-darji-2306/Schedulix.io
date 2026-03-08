06d43b9100da9e6281b5a9ef9b3b6121222c9feb
Prince <pnce18113@gmail.com>
2026-03-08 23:59:41 +0530

    May be changes for deployment and design issues

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-# from fastapi.staticfiles import StaticFiles
 import uvicorn
 from dotenv import load_dotenv
 from api import auth, tasks, notifications
@@ -7,21 +6,7 @@ from api import auth, tasks, notifications
 load_dotenv()
 
 app = FastAPI(title="Schedulix.io")
-# from fastapi.staticfiles import StaticFiles
 
-# class NoCacheStaticFiles(StaticFiles):
-#     async def get_response(self, path, scope):
-#         response = await super().get_response(path, scope)
-#         response.headers["Cache-Control"] = "no-store"
-#         return response
-
-# app.mount("/static", NoCacheStaticFiles(directory="static"), name="static")
-
-# Mount Static Files
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Include Routers
-# app.include_router(pages.router)
 app.include_router(auth.router)
 app.include_router(tasks.router)
 app.include_router(notifications.router)
